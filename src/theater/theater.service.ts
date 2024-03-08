4464d73ee4b054f8e8a0acfbd0958ff91b263d99
이수찬 <lovetmfqud@naver.com>
2024-03-08 12:50:15 +0900

    add: 선호극장 가져오기 추가

--- src/theater/theater.service.ts
+++ src/theater/theater.service.ts
@@ -1,50 +1,60 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
-import theaterList from './theater.data'
+import theaterList from './theater.data';
 import { TheaterResponse } from './interface';
 
 @Injectable()
 export class TheaterService {
-  constructor(
-    private prisma: PrismaService,
-  ) { }
+  constructor(private prisma: PrismaService) {}
 
+  /* data.ts 영화관 데이터 DB Setting */
   async saveTheater() {
     const result = await this.prisma.theater.findFirst();
 
     if (result) return;
 
-    await Promise.allSettled(theaterList.map((ele) => {
-      return this.prisma.theater.create({
-        data: { name: ele.area_depth1 }
-      })
-    }));
+    await Promise.allSettled(
+      theaterList.map((ele) => {
+        return this.prisma.theater.create({
+          data: { name: ele.area_depth1 },
+        });
+      }),
+    );
 
-    return Promise.allSettled(theaterList.map((ele1) => {
-      return ele1.area_depth2.map((ele2) => {
-        return this.prisma.screen.create({
-          data: {
-            name: ele2.txt,
-            theater: {
-              connect: {
-                id: ele1.id
-              }
-            }
-          },
+    return Promise.allSettled(
+      theaterList
+        .map((ele1) => {
+          return ele1.area_depth2.map((ele2) => {
+            return this.prisma.screen.create({
+              data: {
+                name: ele2.txt,
+                lat: ele2.lat,
+                lng: ele2.lng,
+                loc: ele2.loc,
+                theater: {
+                  connect: {
+                    id: ele1.id,
+                  },
+                },
+              },
+            });
+          });
         })
-      })
-    }).reduce((acc, cur) => [...acc, ...cur], []));
+        .reduce((acc, cur) => [...acc, ...cur], []),
+    );
   }
 
+  /* 영화관 정보 가져오기 */
   async getTheater() {
-    return this.prisma.theater.findMany()
+    return this.prisma.theater
+      .findMany()
       .then((theater) => {
         return Promise.allSettled(
           theater.map(async (theater) => {
             const screenList = await this.prisma.screen.findMany({
               where: {
-                theaterId: theater.id
-              }
+                theaterId: theater.id,
+              },
             });
 
             return {
@@ -53,57 +63,66 @@ export class TheaterService {
               area_depth2: screenList.map((screen) => ({
                 id: screen.id,
                 txt: screen.name,
-              }))
-            }
-          })
+                lat: screen.lat,
+                lng: screen.lng,
+                loc: screen.loc,
+              })),
+            };
+          }),
         );
       })
       .then((result) => {
         return result
-          .filter(({ status }) => (
-            status === 'fulfilled'
-          ))
-          .map((result) => (result as PromiseFulfilledResult<TheaterResponse>).value);
+          .filter(({ status }) => status === 'fulfilled')
+          .map(
+            (result) =>
+              (result as PromiseFulfilledResult<TheaterResponse>).value,
+          );
       });
   }
 
   getTheaterList(region?: string) {
-    return this.prisma.theater.findMany({
-      where: {
-        name: {
-          contains: region,
-        }
-      },
-      select: {
-        id: true,
-        name: true,
-        screen: true,
-      }
-    }).then((result) => {
-      return result.map(({ id, name, screen }) => ({
-        id,
-        region: name,
-        theaters: screen.map(({ theaterId, ...rest }) => rest)
-      }))
-    });
+    return this.prisma.theater
+      .findMany({
+        where: {
+          name: {
+            contains: region,
+          },
+        },
+        select: {
+          id: true,
+          name: true,
+          screen: true,
+        },
+      })
+      .then((result) => {
+        return result.map(({ id, name, screen }) => ({
+          id,
+          region: name,
+          theaters: screen.map(({ theaterId, ...rest }) => rest),
+        }));
+      });
   }
 
+  /* 선호 영화관 가져오기 */
   getTheaterById(screenId: number) {
-    return this.prisma.screen.findFirst({
-      where: {
-        id: screenId
-      },
-      include: {
-        theater: true
-      }
-    }).then((result) => {
-      return {
-        id: result.id,
-        name: result.name,
-        region: result.theater.name,
-        location_LO: 127.0263387,
-        location_LA: 37.50166171
-      }
-    })
+    return this.prisma.screen
+      .findFirst({
+        where: {
+          id: screenId,
+        },
+        include: {
+          theater: true,
+        },
+      })
+      .then((result) => {
+        return {
+          id: result.id,
+          name: result.name,
+          region: result.theater.name,
+          location_LO: 127.0263387,
+          location_LA: 37.50166171,
+        };
+      });
   }
-} 
+}
